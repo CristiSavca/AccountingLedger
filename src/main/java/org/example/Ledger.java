3f5efeebc536412f2127f3714357885f4c179cfa
Cristi Savca <cristisavca44@gmail.com>
2023-05-02 16:54:41 -0400

    sorted transactions array, and implemented Ledger Reports Menu with search filters

--- src/main/java/org/example/Ledger.java
+++ src/main/java/org/example/Ledger.java
@@ -5,15 +5,17 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Ledger {
-    public static Scanner scanner = new Scanner(System.in);
-    public static ArrayList<Transaction> transactions = getTransactions();
-    public static ArrayList<Transaction> getTransactions(){
+    public static Scanner scanner = new Scanner(System.in); // initialize a global scanner to avoid redundant Scanner declarations
+    public static ArrayList<Transaction> transactions = getTransactions(); // initialize global array and inherit from getTransactions method
+
+    public static ArrayList<Transaction> getTransactions() { // parse csv file, store transactions into objects, and add to arraylist
         ArrayList<Transaction> transactions = new ArrayList<>();
-        // this method loads transaction objects into transactions array
-        try{
+
+        try {
             FileReader fileReader = new FileReader("transactions.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
 
@@ -33,9 +35,13 @@ public class Ledger {
             System.out.println("File not found!");
             System.exit(0);
         }
+        // Sort our transactions ArrayList in ascending order before returning it
+        Comparator<Transaction> compareByDate = Comparator.comparing(Transaction::getDate).reversed();
+        Comparator<Transaction> compareByTime = Comparator.comparing(Transaction::getTime).reversed();
+        transactions.sort(compareByDate.thenComparing(compareByTime));
+
         return transactions;
     }
-
 
     public static void ledgerMenu() {
         System.out.println("""
@@ -47,7 +53,7 @@ public class Ledger {
                 [R] - Reports
                 [H] - Home""");
         String input = scanner.nextLine();
-        switch(input.toUpperCase()){
+        switch (input.toUpperCase()) {
             case "A" -> showEntries();
             case "D" -> showDeposits();
             case "P" -> showPayments();
@@ -59,13 +65,13 @@ public class Ledger {
 
     public static void showEntries() {
         System.out.println("All Entries:");
-        for (Transaction item : transactions){
+        for (Transaction item : transactions) {
             System.out.println(
                     item.getDate() + " " +
-                    item.getTime() + " " +
-                    item.getDescription() + " " +
-                    item.getVendor() + " " +
-                    item.getAmount()
+                            item.getTime() + " " +
+                            item.getDescription() + " " +
+                            item.getVendor() + " $" +
+                            item.getAmount()
             );
         }
         System.out.println("[X] - Exit");
@@ -79,49 +85,116 @@ public class Ledger {
 
     public static void showDeposits() {
         System.out.println("All Deposits:");
-        for (Transaction item : transactions){
+        for (Transaction item : transactions) {
             if (item.getAmount() > 0) {
                 System.out.println(
                         item.getDate() + " " +
                                 item.getTime() + " " +
                                 item.getDescription() + " " +
-                                item.getVendor() + " " +
+                                item.getVendor() + " $" +
                                 item.getAmount()
                 );
             }
         }
         System.out.println("[X] - Exit");
         String input = scanner.nextLine();
-        if (input.equalsIgnoreCase("X")) {
-            ledgerMenu();
-        } else {
-            ledgerMenu();
-        }
+        if (input.equalsIgnoreCase("X")) {ledgerMenu();}
+        else {ledgerMenu();}
     }
 
     public static void showPayments() {
         System.out.println("All Payments:");
-        for (Transaction item : transactions){
+        for (Transaction item : transactions) {
             if (item.getAmount() < 0) {
                 System.out.println(
                         item.getDate() + " " +
                                 item.getTime() + " " +
                                 item.getDescription() + " " +
-                                item.getVendor() + " " +
+                                item.getVendor() + " $" +
                                 item.getAmount()
                 );
             }
         }
         System.out.println("[X] - Exit");
         String input = scanner.nextLine();
-        if (input.equalsIgnoreCase("X")) {
-            ledgerMenu();
-        } else {
-            ledgerMenu();
-        }
+        if (input.equalsIgnoreCase("X")) {ledgerMenu();}
+        else {ledgerMenu();}
     }
 
     public static void reportsMenu() {
+        System.out.println("""
+                Reports Menu:
+                [1] - Month To Date
+                [2] - Previous Month
+                [3] - Year To Date
+                [4] - Previous Year
+                [5] - Search By Vendor
+                [0] - Back""");
+        LocalDate date = LocalDate.now();
+        String filter = "";
+        String input = scanner.nextLine();
+        switch (input.toUpperCase()) {
+            case "1" -> {date = LocalDate.now().withDayOfMonth(1);
+                         filter = "Month To Date";}
+            case "2" -> {date = LocalDate.now().minusMonths(1).withDayOfMonth(1);
+                         filter = "Previous Month";}
+            case "3" -> {date = LocalDate.now().withDayOfYear(1);
+                         filter = "Year To Date";}
+            case "4" -> {date = LocalDate.now().minusYears(1).withDayOfYear(1);
+                         filter = "Previous Year";}
+            case "5" -> {System.out.println("Enter Vendor Name:");
+                         String vendor = scanner.nextLine();
+                         searchByVendor(vendor);}
+            case "0" -> ledgerMenu();
+            default -> System.out.println("Please enter a valid option");
+        }
+        reportsLoops(date, filter);
+    }
 
+    public static void reportsLoops(LocalDate filterDate, String filterType) {
+        int itemDateVal = 0;
+        int searchBy = 0;
+        System.out.println(filterType + " Transactions");
+
+        for (Transaction item : transactions) {
+            if (filterType.equals("Month To Date") || filterType.equals("Previous Month")){
+                itemDateVal = item.getDate().getMonthValue();
+                searchBy = filterDate.getMonthValue();
+            } else {itemDateVal = item.getDate().getYear();
+                    searchBy = filterDate.getYear();}
+
+            if (itemDateVal == searchBy) {
+                System.out.println(
+                        item.getDate() + " " +
+                                item.getTime() + " " +
+                                item.getDescription() + " " +
+                                item.getVendor() + " $" +
+                                item.getAmount()
+                );
+            }
+        }
+        System.out.println("[X] - Exit");
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("X")) {ledgerMenu();}
+        else {ledgerMenu();}
+    }
+
+    public static void searchByVendor(String vendor) {
+        System.out.println("All " + vendor + " Transactions:");
+        for (Transaction item : transactions) {
+            if (item.getVendor().equals(vendor)) {
+                System.out.println(
+                        item.getDate() + " " +
+                                item.getTime() + " " +
+                                item.getDescription() + " " +
+                                item.getVendor() + " $" +
+                                item.getAmount()
+                );
+            }
+        }
+        System.out.println("[X] - Exit");
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("X")) {ledgerMenu();}
+        else {ledgerMenu();}
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,14 @@ package org.example;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        homeScreen();
-    }
-    public static void homeScreen() {
+    public static void main(String[] args) {homeScreen();} // run home screen
+
+    public static void homeScreen() { // print home menu choices
         System.out.println("""
                 Welcome to your Finance Account!
                 Main Menu:
@@ -18,20 +18,16 @@ public class Main {
                 [L] - Ledger
                 [X] - Exit""");
         String input = scanner.nextLine();
-        switch (input.toUpperCase()) {
+        switch (input.toUpperCase()) { // run corresponding methods (screens) based on user input
             case "D" -> addDeposit();
             case "P" -> makePayment();
-            case "L" -> showLedger();
+            case "L" -> Ledger.ledgerMenu();
             case "X" -> System.exit(0);
             default -> System.out.println("Please enter a valid option");
         }
     }
 
-    public static void addDeposit() {
-        System.out.println("Enter Date: YYYY-MM-DD");
-        String date = scanner.nextLine();
-        System.out.println("Enter Time: HH:MM:SS");
-        String time = scanner.nextLine();
+    public static void addDeposit() { // prompt user for details of transaction and store it in corresponding variables
         System.out.println("Enter Description:");
         String description = scanner.nextLine();
         System.out.println("Enter Vendor:");
@@ -39,29 +35,20 @@ public class Main {
         System.out.println("Enter Deposit Amount:");
         double amount = scanner.nextDouble();
 
-        try (FileWriter fileWriter = new FileWriter("transactions.csv", true)){
+        // write the variables' info to the csv file with appropriate format and use current date/time
+        try (FileWriter fileWriter = new FileWriter("transactions.csv", true)) {
             fileWriter.write("\n" +
-                    date + "|" +
-                    time + "|" +
-                    description + "|" +
-                    vendor + "|" +
-                    amount
-                    );
+                    LocalDate.now() + "|" + LocalTime.now() + "|" + description + "|" + vendor + "|" + amount
+            );
             fileWriter.close();
             System.out.println("Deposit added successfully!");
-        }
-        catch(IOException e){
+        } catch (IOException e) { // throw error message when input is erroneous
             System.out.println("Error inputting data!");
         }
-        homeScreen();
+        homeScreen(); // run the home screen menu again when finished adding deposit
     }
 
-
-    public static void makePayment() {
-        System.out.println("Enter Date: YYYY-MM-DD");
-        String date = scanner.nextLine();
-        System.out.println("Enter Time: HH:MM:SS");
-        String time = scanner.nextLine();
+    public static void makePayment() { // Same as deposit method but add a "-" sign before the amount
         System.out.println("Enter Description:");
         String description = scanner.nextLine();
         System.out.println("Enter Vendor:");
@@ -69,24 +56,15 @@ public class Main {
         System.out.println("Enter Transaction Amount:");
         double amount = scanner.nextDouble();
 
-        try (FileWriter fileWriter = new FileWriter("transactions.csv", true)){
+        try (FileWriter fileWriter = new FileWriter("transactions.csv", true)) {
             fileWriter.write("\n" +
-                    date + "|" +
-                    time + "|" +
-                    description + "|" +
-                    vendor + "|" + "-" +
-                    amount
+                    LocalDate.now() + "|" + LocalTime.now() + "|" + description + "|" + vendor + "|-" + amount
             );
             fileWriter.close();
             System.out.println("Payment made successfully!");
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Error inputting data!");
         }
         homeScreen();
-    }
-
-    public static void showLedger() {
-        Ledger.ledgerMenu();
     }
 }
